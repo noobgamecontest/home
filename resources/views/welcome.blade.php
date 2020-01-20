8dafd4b3d6e42a6ef6c98d5a2fbdb2c22ce9b42d
William <work@suppo.fr>
2020-01-20 22:09:23 +0100

    Suppression hamburger.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -54,12 +54,6 @@
             </a>
         </div>
 
-        <div class="block lg:hidden pr-4">
-            <button id="nav-toggle" class="flex items-center p-1 text-orange-800 hover:text-gray-900">
-                <svg class="fill-current h-6 w-6" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
-            </button>
-        </div>
-
         <div class="w-full flex-grow lg:flex lg:items-center lg:w-auto hidden lg:block mt-2 lg:mt-0 bg-white lg:bg-transparent text-black p-4 lg:p-0 z-20" id="nav-content">
             <ul class="list-reset lg:flex justify-end flex-1 items-center">
                 <li class="mr-3">
